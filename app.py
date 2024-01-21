282db34ae0143263f944ab09f0693e14288845f9
Ricky Ringler <135162902+rickyringler@users.noreply.github.com>
2024-01-21 10:59:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
+#Ricky Ringler : AffirME
+#Repo : https://github.com/rickyringler/AffirME
+#Documentation : https://github.com/rickyringler/AffirME/blob/master/README.md
+
 from flask import Flask, render_template
 from task_scheduler import *
 app = Flask(__name__)
-
 
 @app.route("/")
 def home():
@@ -23,3 +26,27 @@ def yoga():
 
 if __name__=="__main__":
     app.run(debug=True)
+
+'''
+MIT License
+
+Copyright (c) [Year] [Your Name]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
